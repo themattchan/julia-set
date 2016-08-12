7c96fb7c4142ba662759895001049c845db4683f
Matt Chan <mattchan@awakenetworks.com>
2016-08-12 00:22:36 -0700

    flub

---
+++ README.hs
@@ -0,0 +1,6 @@
+julia-set
+=========
+
+Trippy Julia Fractals ([Reddit Daily Programmer #277](https://www.reddit.com/r/dailyprogrammer/comments/4v5h3u/20160729_challenge_277_hard_trippy_julia_fractals/))
+
+This is a redo after a SSD crash :(
